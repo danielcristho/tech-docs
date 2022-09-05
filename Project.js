34aac868c5ca7b48f7f51287ea46e61f8820b5dd
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-09-06 01:32:45 +0700

    feat: Update navbar

---
+++ Project.js
@@ -0,0 +1,10 @@
+let projectData = [
+    {
+        image: 'img/project-1.png',
+        name: 'project one',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cong',
+        github: '#',
+        live: '#',
+        tags: '#python, #backend, #devops'
+    },
+]
