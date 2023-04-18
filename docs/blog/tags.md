3da81755aa09e0a391f7752cd5ff54620941b3b3
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-04-19 00:08:32 +0700

    feat: update dev

---
+++ docs/blog/tags.md
@@ -0,0 +1,9 @@
+---
+title: Tags
+description: Tags and list of pages
+template: tags.html
+disqus: "danielcristho-github-io"
+hide:
+    - navigation
+    - toc
+---
---
+++ overrides/assets/extra.js
@@ -0,0 +1,11 @@
+/* open external links in new tab */
+var links = document.links;
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (links[i].hostname != window.location.hostname) {
+        links[i].target = "_blank";
+        links[i].setAttribute("rel", "noopener noreferrer");
+        links[i].className += " externalLink";
+    } else {
+        links[i].className += " localLink";
+    }
+}
